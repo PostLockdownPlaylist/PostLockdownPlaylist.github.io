f3f42db69a4f6794ece4cd140fd88254ef4cb600
PostLockdownPlaylist <78348327+PostLockdownPlaylist@users.noreply.github.com>
2021-02-04 18:00:56 +0000

    Add files via upload

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -2,7 +2,6 @@ const userID = 'w8zzqofg0qzyeaw5nptn9fi3t';
 const playlistId = '6qCbpzHdIEWoLJY9PF0rtD'; // actual one is 14M5jiWFCPq9bWpd6sUmDL
 const clientId = 'df4fdffc7f5948d4bd55ec5edb828ae4';
 
-const tokenURL = 'https://postlockdownplaylist.github.io/#access_token=BQDqUmcZHepyJGKnrDB13gVwQpfxIeYhMyQ2kJ9p45_SmLBntKT_TXSoJS0sGityHqc8NPAN9ThYooTTuaDMi4IK3fjfIC9RvI6G6lYhLXnGXswXUS7_iTf6_KzitLMR2mtbdFRy5snrAPKJxotDcGa5XwB-Jk6mYhrfr6lQe0pq9Xhu5pC8tQqc2pgFTrtid9rSBuaa_cNM&token_type=Bearer&expires_in=3600';
 
 
 const initPage = () => {
@@ -21,12 +20,20 @@ const setLoginURL = () => {
     console.log(loginURL);
 }
 
+
+const initTokenProcess = () => {
+	const token = getToken();
+	console.log(token)
+}
+
+
 const getToken = () => {
+	const tokenURL = window.location.href;
 	const myRegex = /\#(access_token)\=(\S*?)\&/;
 	matchArr = tokenURL.match(myRegex);
 	const token = matchArr[2];
 	console.log(token);
-	console.log(window.location.href);
+	return token;
 }
 
 
